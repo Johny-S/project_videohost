dac9de2a390d2ec8251c2dfa21cd48cc2eadae94
Evgenii Sumaev <9364016@gmail.com>
2019-10-11 14:00:39 +0300

    Release 1

---
+++ public/js/application.js
@@ -0,0 +1,117 @@
+// import { URL } from "url"
+const startButton = document.querySelector('.start');
+const rec = document.querySelector('.rec');
+const download = document.querySelector('.download');
+
+// Таймер
+const timerBox = document.getElementById('timer');
+
+const startTimer = {
+  timer: null,
+  go(time, mediaRecorder) {
+    let sec = `${time}`;
+    timerBox.innerHTML = sec;
+    this.timer = setInterval(() => {
+      sec = +sec < 11 ? `0${sec - 1}` : `${sec - 1}`;
+      timerBox.innerHTML = sec;
+      if (sec === '00') {
+        clearInterval(this.timer);
+        goStop(mediaRecorder);
+      }
+    }, 1e3);
+  },
+  stop() {
+    clearInterval(this.timer);
+    timerBox.innerHTML = '00';
+  }
+};
+
+// проверка поддерживает ли браузер navigator.mediaDevices.getUserMedia
+const hasGetUserMedia = () => {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+};
+
+if (hasGetUserMedia()) {
+  console.log('works');
+} else {
+  alert('Your brouser is not supported video stream');
+}
+
+// доступ к камере
+
+let recBlobs = [];
+
+const video = document.querySelector('video');
+console.log(video);
+
+startButton.onclick = () => userStream();
+
+const goRec = mediaRecorder => {
+  // rec.style.background = 'red';
+  rec.style.color = 'red';
+  rec.innerHTML = '&#149 rec &#149';
+  rec.onclick = () => goStop(mediaRecorder);
+  mediaRecorder.start(1e10);
+  mediaRecorder.ondataavailable = e => recBlobs.push(e.data);
+  console.log('MediaRecorder started', mediaRecorder);
+  mediaRecorder.requestData();
+  console.log(mediaRecorder.state);
+  // stop.disabled = false;
+  // rec.disabled = true;
+  startTimer.go(10, mediaRecorder);
+};
+
+const goStop = mediaRecorder => {
+  mediaRecorder.stop();
+  rec.onclick = () => goRec(mediaRecorder);
+  // rec.style.background = '';
+  rec.style.color = '';
+  rec.innerHTML = 'rec';
+  startTimer.stop();
+  console.log(mediaRecorder.state);
+  console.log('recorder stopped');
+  console.log(recBlobs);
+};
+
+const goStream = stream => {
+  const mediaRecorder = new MediaRecorder(stream);
+
+  rec.onclick = () => goRec(mediaRecorder);
+
+  video.srcObject = stream;
+};
+
+const userStream = () => {
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: true
+    })
+    .then(goStream);
+};
+
+download.addEventListener('click', async e => {
+  e.preventDefault();
+  console.log('send');
+
+  let fd = new FormData();
+  fd.append('recfile', recBlobs[1], 'recfile');
+
+  const response = await fetch('/upload', {
+    method: 'POST',
+    body: fd,
+    headers: {
+      Accept: 'application/jsone'
+    }
+  });
+});
+
+// download.onclick = () => {
+//   let link = document.createElement('a');
+//   link.download = 'hello.webm';
+//   link.href = URL.createObjectURL(recBlobs[1]);
+
+//   link.click();
+
+//   URL.revokeObjectURL(link.href);
+// };
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
 
-router.get('/', function (req, res, next) {
+const upload = multer({ dest: __dirname + '/video' });
+const type = upload.single('recfile');
+
+router.get('/', function(req, res, next) {
   res.render('entries/index', { mainOff: true });
 });
 
-router.get('/new', function (req, res, next) {
+router.get('/new', function(req, res, next) {
   res.render('users/new');
+});
+
+router.post('/upload', type, (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  res.send();
 });
 
 module.exports = router;
